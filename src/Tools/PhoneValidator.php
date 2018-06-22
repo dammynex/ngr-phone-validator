993ce0a4f18cefa6e6194998fb3a53a05150d4f0
Dammy Brainlord <dammybrainlord@gmail.com>
2018-06-22 08:38:34 +0100

    Moved network related properties to PhoneNetworkParser Class

--- src/Tools/PhoneValidator.php
+++ src/Tools/PhoneValidator.php
@@ -7,58 +7,6 @@ use SebastianBergmann\Diff\Parser;
 
 class PhoneValidator
 {
-    //Networks
-    const NETWORK_MTN = 'MTN';
-    const NETWORK_AIRTEL = 'AIRTEL';
-    const NETWORK_9MOBILE = '9MOBILE';
-    const NETWORK_GLO = 'GLO';
-
-    /**
-     * Major phone number prefixes
-     *
-     * @see https://en.wikipedia.org/wiki/Telephone_numbers_in_Nigeria
-     * @var array
-     */
-    private $prefixes = array(
-        self::NETWORK_MTN => array(
-            '0703',
-            '0706',
-            '0803',
-            '0806',
-            '0813',
-            '0814',
-            '0816',
-            '0903'
-        ),
-
-        self::NETWORK_AIRTEL => array(
-            '0701',
-            '0708',
-            '0802',
-            '0808',
-            '0812',
-            '0902',
-            '0907'
-        ),
-
-        self::NETWORK_GLO => array(
-            '0705',
-            '0805',
-            '0807',
-            '0811',
-            '0815',
-            '0905'
-        ),
-
-        self::NETWORK_9MOBILE => array(
-            '0809',
-            '0817',
-            '0818',
-            '0909',
-            '0908'
-        )
-    );
-
     /**
      * Phone number to validate
      *
